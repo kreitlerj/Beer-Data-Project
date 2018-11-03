fdf4acd52878794b7fbdf91a0d860e4c1ead6f9c
Carlos Soriano <soriano.carlitos@gmail.com>
2018-11-03 11:13:00 -0500

    fixed my local repo and commiting changes to push in class 11_3

---
+++ config.py
@@ -0,0 +1,3 @@
+API_KEY = "tXdwvpJTnm5uq0ruhAsMx7fDYaNCjsOpbMkSOOxqbtOSOUtOjIZ2twcCAbhpHuloTD7sn3RMjsHcLxM73qOmgZUuggweNxUYhl1AAsTWQESSVhCIaQEEBJyld_DcW3Yx"
+
+gkey = "AIzaSyDurE5hYJQkp9MzOEr7WjvLzsRlLpkMsjI"
