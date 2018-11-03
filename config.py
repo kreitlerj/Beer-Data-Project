438971f9b961295410a26e7108f1cd955fed56f2
Carlos Soriano <soriano.carlitos@gmail.com>
2018-11-03 12:43:29 -0500

    in class added google for loop and config to gitignore

--- config.py
+++
@@ -1,3 +0,0 @@
-API_KEY = "tXdwvpJTnm5uq0ruhAsMx7fDYaNCjsOpbMkSOOxqbtOSOUtOjIZ2twcCAbhpHuloTD7sn3RMjsHcLxM73qOmgZUuggweNxUYhl1AAsTWQESSVhCIaQEEBJyld_DcW3Yx"
-
-gkey = "AIzaSyDurE5hYJQkp9MzOEr7WjvLzsRlLpkMsjI"
